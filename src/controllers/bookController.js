93f09ce846fd85c05507d19131be25b192435333
nickgautam <er.nishantgautam0011@gmail.com>
2022-06-10 03:31:41 +0530

    logic is running but only in happy flow

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,31 @@
 const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
-
+const publisherModel= require("../models/publisherModel")
 const createBook= async function (req, res) {
     let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
-}
+   if(!req.body.author_id) {
+    res.send({msg: "author_id is required"})
+   }
+   else if(!authorModel.find(req.body.author_id)) {
+  res.send ({msg: "author is not present with this id"})
+   }
+    else if(!req.body.publisher_id){
+     res.send({msg: "publisher_id is required"})  
+   }
+   else if(!publisherModel.find(req.body.publisher_id)) {
+    res.send({msg: "publisher is not present with this id"})
+   }
+     else { let bookCreated = await bookModel.create(book)
+        res.send({data: bookCreated})
+    }
+ }  
 
-const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
-    res.send({data: books})
-}
+module.exports.createBook= createBook
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
+const getBooksWithcompleteDetails = async function (req, res) {
+    let specificBook = await bookModel.find().populate('author_id','publisher_id')
     res.send({data: specificBook})
 
 }
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.getBooksWithcompleteDetails= getBooksWithcompleteDetails
