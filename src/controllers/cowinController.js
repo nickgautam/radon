17b87ef48d524fff1395aa851cffc77ce689b89f
nickgautam <er.nishantgautam0011@gmail.com>
2022-06-18 00:52:49 +0530

    17/06/2022 assignment done

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -134,11 +134,10 @@ try{
     template_id= req.query.template_id
     text0= req.query.text0
     text1= req.query.text1
-    username= req.query.username
-    password= req.query.password
+   
 let options= {
     method: "post",
-    url: `https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`,
+    url: `https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=chewie12345&password=meme@123`,
 }
 
 let result= await axios(options)
